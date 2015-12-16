1a85ff79dc68e556b0ff3539eebc136b7ba4667d
Mihai Corcosa <corcosa.mihai@isoftbet.com>
2015-12-16 13:31:00 +0200

    changed the way the input is serialized

--- codeadvent.js
+++ codeadvent.js
@@ -1,23 +1,47 @@
 var fs = require('fs');
 var array = fs.readFileSync('inputs/day7.txt').toString().split(";");
 
-//split each expressions into atoms and reverse them, so the output is the first element of the array
-for(i in array) {
-    array[i] = array[i].split(' ');
-    array[i] = array[i].reverse();
-    array[i].result = array[i][0];
-    array[i].operation = array[i][3];
-    array[i].operand1 = array[i][2];
-    if(!isNaN(array[i].operand1))
-        array[i].operand1=parseInt(array[i].operand1)
-    array[i].operand2 = array[i][4];
-    if(!isNaN(array[i].operand2))
-        array[i].operand2=parseInt(array[i].operand2)
+function serializeData(){
+    //split each expressions into atoms and make a wire object
+    for(i in array) {
+        array[i] = array[i].split(' ');
+        array[i] = array[i].reverse();
 
-    for(var j=0;j<5;j++)
-        array[i].splice(0,5);
+        array[i].identifier = array[i][0];
+        array[i].gate = array[i][3];
+        array[i].source1 = array[i][2];
+        array[i].signal = null;
+        if(!isNaN(array[i].source1))
+            array[i].source1 = parseInt(array[i].source1);
+        array[i].source2 = array[i][4];
+        if(!isNaN(array[i].source2))
+            array[i].source2 = parseInt(array[i].source2);
+
+        for(var j=0;j<5;j++)
+            array[i].splice(0,5);
+
+        //change undefined values to null
+        for(var key in array[i]){
+            if (typeof array[i][key] == 'undefined') {
+                array[i][key] = null;
+            }
+        }
+    }
 }
 
 function evalWire(wire){
-
+   console.log(wire);
+    switch (wire.gate){
+        case null:
+            console.log("BEFORE: "+wire.signal);
+            //console.log('undefined');
+            wire.signal = evalWire(wire.source1);
+            console.log("AFTER: "+wire.signal);
+            break;
+    }
 }
+
+serializeData();
+for(i in array) {
+evalWire(array[i])}
+//console.log(array);
